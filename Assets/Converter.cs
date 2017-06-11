53052ac294a8afaf01007997b574d8846958175e
kcozy <s1713089@coins.tsukuba.ac.jp>
2017-06-11 21:48:51 +0900

    Initial commit

---
+++ Assets/Converter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Converter : MonoBehaviour
+{
+
+    public InputField F, C;
+    public static string stringC, stringF;
+    public static int Change;
+
+    public static double FtoC(double f)
+    {
+        return (f - 32.0) / 1.8;
+    }
+
+    public static double CtoF(double c)
+    {
+        return 1.8 * c + 32.0;
+    }
+
+    public void Convert(int ForC)
+    {
+        if (ForC == 1)
+        {
+            C.text = FtoC(double.Parse(F.text)).ToString();
+        }
+        else
+        {
+            F.text = CtoF(double.Parse(C.text)).ToString();
+        }
+    }
+
+    public void Start()
+    {
+        Change = -1;
+    }
+    void Update()
+    {
+        if (Change != -1)
+        {
+            Convert(Change);
+            Change = -1;
+        }
+    }
+}
---
+++ Assets/SendString.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendString : MonoBehaviour
+{
+
+    // 1 -> F, 0 -> C
+    public bool CF;
+
+    public void ValueChanged(string text)
+    {
+        double result;
+        bool isValid = double.TryParse(text, out result);
+        if (isValid)
+        {
+            if (CF)
+            {
+                Converter.stringF = text;
+                Converter.Change = 1;
+            }
+            else
+            {
+                Converter.stringC = text;
+                Converter.Change = 0;
+            }
+        }
+    }
+
+}
